937cae75eabaf59763c970980762437f324b61f3
Vasilii Pochkaenko <vassili.pochkaenko@vitec.com>
2022-11-29 17:16:28 +0700

    feat: add stop by signals to example 'hello wolrd'

--- examples/helloWorld/main.cpp
+++ examples/helloWorld/main.cpp
@@ -1,6 +1,8 @@
 #include <Coroutine03/Core/Cin.h>
 #include <Coroutine03/Core/Cout.h>
 #include <Coroutine03/Core/DicpatcherEPoll.h>
+#include "Coroutine03/Core/StopBySignals.h"
+#include <exception>
 #include <iostream>
 #include <Poco/SharedPtr.h>
 #include <string>
@@ -8,17 +10,23 @@
 using Coroutine03::Core::Cin;
 using Coroutine03::Core::Cout;
 using Coroutine03::Core::DispatcherEPoll;
+using Coroutine03::Core::StopBySignals;
 using Poco::SharedPtr;
 using std::endl;
 using std::string;
 
 int main() {
-	SharedPtr<DispatcherEPoll> dispatcher = new DispatcherEPoll();
-	Cin cin(dispatcher);
-	Cout cout(dispatcher);
-	cout << "What is your name?" << endl;
-	string name;
-	cin >> name;
-	cout << "Hello " << name << "!" << endl;
+	try {
+		SharedPtr<DispatcherEPoll> dispatcher = new DispatcherEPoll();
+		StopBySignals appExit(dispatcher);
+		Cin cin(dispatcher);
+		Cout cout(dispatcher);
+		cout << "What is your name?" << endl;
+		string name;
+		cin >> name;
+		cout << "Hello " << name << "!" << endl;
+	} catch(const DispatcherEPoll::CanceledException &ex) {
+		std::cout << "Stop: " << ex.what() << endl;
+	}
 	return 0;
 }
